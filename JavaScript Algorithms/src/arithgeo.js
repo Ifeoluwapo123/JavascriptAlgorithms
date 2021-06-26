07b37b13b1374154399a933754a0a5d02b2c4f58
ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-06-27 00:46:55 +0100

    added src folder

---
+++ JavaScript Algorithms/src/arithgeo.js
@@ -0,0 +1,17 @@
+/**check if the sequence of integer in array is geometric or arithmetic */
+function arithGeo(arr){
+    const arithDiff = arr[1] - arr[0];
+    const geoRatio = arr[1] / arr[0];
+    var arithRes = 0, geoRes = 0;
+
+    for(var i = 2; i < arr.length; i++){
+        if(arr[i] - arr[i-1] !== arithDiff) arithRes++;
+        if(arr[i] / arr[i-1] !== geoRatio) geoRes++;
+    }
+
+    if(arithRes === 0) return 'arithmetic';
+    if(geoRes === 0) return 'geometric';
+    if(arithRes !== 0 && geoRes !== 0) return false;
+}
+
+console.log(arithGeo([2, 4, 8, 16]));
