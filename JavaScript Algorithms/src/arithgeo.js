bc7323fa04a73e3e47760052785341ecf365395a
ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-06-27 00:54:24 +0100

    made code accessible in the root file

--- JavaScript Algorithms/src/arithgeo.js
+++ JavaScript Algorithms/src/arithgeo.js
@@ -1,17 +1,18 @@
 /**check if the sequence of integer in array is geometric or arithmetic */
-function arithGeo(arr){
-    const arithDiff = arr[1] - arr[0];
-    const geoRatio = arr[1] / arr[0];
-    var arithRes = 0, geoRes = 0;
+function arithGeo(arr) {
+  const arithDiff = arr[1] - arr[0]
+  const geoRatio = arr[1] / arr[0]
+  var arithRes = 0,
+    geoRes = 0
 
-    for(var i = 2; i < arr.length; i++){
-        if(arr[i] - arr[i-1] !== arithDiff) arithRes++;
-        if(arr[i] / arr[i-1] !== geoRatio) geoRes++;
-    }
+  for (var i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== arithDiff) arithRes++
+    if (arr[i] / arr[i - 1] !== geoRatio) geoRes++
+  }
 
-    if(arithRes === 0) return 'arithmetic';
-    if(geoRes === 0) return 'geometric';
-    if(arithRes !== 0 && geoRes !== 0) return false;
+  if (arithRes === 0) return 'arithmetic'
+  if (geoRes === 0) return 'geometric'
+  if (arithRes !== 0 && geoRes !== 0) return false
 }
 
-console.log(arithGeo([2, 4, 8, 16]));
+module.exports = arithGeo
