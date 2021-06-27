46fdae70374485c96e1d85e7c3e7b3a4ab227d78
ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-06-27 22:48:05 +0100

    letter changes commit

---
+++ JavaScript Algorithms/src/letterchanges.js
@@ -0,0 +1,18 @@
+/**
+ * check each character then replace with the next alphabelt, if z return A
+ * and invert all vowel letters to uppercase 
+ */
+function Letterchanges(str){
+    //return String.fromCharCode('a'.charCodeAt()+1)
+    var converted = str.replace(/[a-z]/g, char =>{
+        return (char === 'z' || char === 'Z') ? 'a': String.fromCharCode(char.charCodeAt()+1);
+    });
+
+    var capitalized = converted.replace(/a|e|i|o|u/gi, (vowel)=>{
+        return vowel.toUpperCase();
+    });
+
+    return capitalized;
+}
+
+console.log(Letterchanges('zwealth'));
