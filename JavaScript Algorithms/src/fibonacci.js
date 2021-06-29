cb6a7697f75b75b3b7e9140bc98710aa729f2148
ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-06-30 00:01:07 +0100

    fibonnaci commit added comment

--- JavaScript Algorithms/src/fibonacci.js
+++ JavaScript Algorithms/src/fibonacci.js
@@ -1,3 +1,8 @@
+/**
+ * @param  {//sequence [0, 1, 1, 2, 3, 5, 13, 21, ...]
+ * num as the index} num
+ * @returns 
+ */
 function fibonacci(num) {
   if (num < 2) return num
   return fibonacci(num - 1) + fibonacci(num - 2)
@@ -5,5 +10,6 @@ function fibonacci(num) {
 
 console.log(fibonacci(2))
 
-//sequence [0, 1, 1, 2, 3, 5, 13, 21, ...]
-module.exports = fibonacci;
+
+
+module.exports = fibonacci
